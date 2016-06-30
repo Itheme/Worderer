2657ddaebcc153d8655917354c52faeae878ab2e
Dan Parchomenko <thereaintnomailformeagain@gmail.com>
2016-06-30 19:46:10 +0300

    + Grapheme support

--- Worderer/Worderer/main.m
+++ Worderer/Worderer/main.m
@@ -13,11 +13,13 @@ typedef enum : NSUInteger {
     InputModeBroken = 0,
     InputModeHelp = 1,
     InputModeResourcesToJSON = 2,
-    InputModeResourcesToCode = 3,
-    InputModeResourcesToRandom = 4,
-    InputModeFileToJSON = 5,
-    InputModeFileToCode = 6,
-    InputModeFileToRandom = 7
+    InputModeResourcesToXML = 3,
+    InputModeResourcesToCode = 4,
+    InputModeResourcesToRandom = 5,
+    InputModeFileToJSON = 6,
+    InputModeFileToXML = 7,
+    InputModeFileToCode = 8,
+    InputModeFileToRandom = 9
 } InputMode;
 
 InputMode validateInput(int argc, const char * argv[], NSString **inputFile, NSInteger *genCount) {
@@ -28,6 +30,7 @@ InputMode validateInput(int argc, const char * argv[], NSString **inputFile, NSI
     BOOL json = false;
     BOOL code = false;
     BOOL generate = false;
+    BOOL graphml = false;
     for (NSInteger i = 1; i < argc; i++) {
         NSString *string = [NSString stringWithCString:argv[i] encoding:NSASCIIStringEncoding];
         if ([string isEqualToString:@"-h"]) {
@@ -35,6 +38,10 @@ InputMode validateInput(int argc, const char * argv[], NSString **inputFile, NSI
         }
         if ([string isEqualToString:@"-json"]) {
             json = true;
+            continue;
+        }
+        if ([string isEqualToString:@"-graphml"]) {
+            graphml = true;
             continue;
         }
 //        if ([string isEqualToString:@"-c"]) {
@@ -69,6 +76,9 @@ InputMode validateInput(int argc, const char * argv[], NSString **inputFile, NSI
                     if (code) {
                         return InputModeFileToCode;
                     }
+                    if (graphml) {
+                        return InputModeFileToXML;
+                    }
                     return InputModeFileToJSON;
                 }
                 printf("%s", [[NSString stringWithFormat:@"File %@ found\n", args[1]] cStringUsingEncoding:NSASCIIStringEncoding]);
@@ -84,9 +94,73 @@ InputMode validateInput(int argc, const char * argv[], NSString **inputFile, NSI
     if (code) {
         return InputModeResourcesToCode;
     }
+    if (graphml) {
+        return InputModeResourcesToXML;
+    }
     return InputModeResourcesToJSON;
 }
 
+
+void collectTreeMap(CharTreeLayer *tree, NSMutableArray <CharTreeLayer *> **allObjects) {
+    BOOL found = NO;
+    for (CharTreeLayer *t in *allObjects) {
+        if ([t isEqual:tree]) {
+            found = YES;
+            break;
+        }
+    }
+    if (!found) {
+        [*allObjects addObject:tree];
+    }
+    NSMutableArray *subs = [tree performSelector:@selector(subTree)];
+    for (CharTreeLayer *sub in subs) {
+        collectTreeMap(sub, allObjects);
+    }
+}
+
+NSXMLDocument *makeGraphML(CharTreeLayer *tree) {
+    NSXMLElement *root = [[NSXMLElement alloc] init];
+    NSXMLDocument *doc = [[NSXMLDocument alloc] initWithRootElement:root];
+    doc.version = @"1.0";
+    doc.characterEncoding = @"UTF-8";
+    root.name = @"graphml";
+    [root setAttributesAsDictionary:@{@"xmlns": @"http://graphml.graphdrawing.org/xmlns",
+                                      @"xmlns:xsi": @"http://www.w3.org/2001/XMLSchema-instance",
+                                      @"xsi:schemaLocation": @"http://graphml.graphdrawing.org/xmlns\nhttp://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd"}];
+    NSXMLElement *graph = [[NSXMLElement alloc] initWithName:@"graph"];
+    [graph setAttributesAsDictionary:@{@"id": @"chars",
+                                       @"edgedefault": @"directed"}];
+    [root addChild:graph];
+    NSMutableArray <CharTreeLayer *> *allObjects = [NSMutableArray array];
+    collectTreeMap(tree, &allObjects);
+    [allObjects enumerateObjectsUsingBlock:^(CharTreeLayer * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSXMLElement *node = [NSXMLElement elementWithName:@"node"];
+        unichar c = (unichar)[obj performSelector:@selector(c)];
+        [node setAttributesAsDictionary:@{@"id": [NSString stringWithFormat:@"n%ld", idx],
+                                          @"label": [NSString stringWithFormat:@"%c", c]}];
+        [graph addChild:node];
+    }];
+    __block NSInteger edgeNumber = 0;
+    for (NSInteger sourceIdx = 0; sourceIdx < allObjects.count; sourceIdx++) {
+        CharTreeLayer *source = allObjects[sourceIdx];
+        NSMutableArray *subs = [source performSelector:@selector(subTree)];
+        for (CharTreeLayer *sub in subs) {
+            for (NSInteger tagetIdx = 0; tagetIdx < allObjects.count; tagetIdx++) {
+                CharTreeLayer *target = allObjects[tagetIdx];
+                if ([target isEqual:sub]) {
+                    NSXMLElement *edge = [NSXMLElement elementWithName:@"edge"];
+                    [edge setAttributesAsDictionary:@{@"id": [NSString stringWithFormat:@"e%ld", edgeNumber++],
+                                                      @"source": [NSString stringWithFormat:@"n%ld", sourceIdx],
+                                                      @"target": [NSString stringWithFormat:@"n%ld", tagetIdx]}];
+                    [graph addChild:edge];
+                    break;
+                }
+            }
+        }
+    }
+    
+    return doc;
+}
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSString *fileName = nil;
@@ -98,7 +172,7 @@ int main(int argc, const char * argv[]) {
         if (mode == InputModeHelp) {
             printf("Usage: \n");
             printf("  Worderer [-h]\n");
-            printf("  Worderer [-i wordlist.txt] [{-json|-g [count]}]\n");
+            printf("  Worderer [-i wordlist.txt] [{-json|-graphml|-g [count]}]\n");
             printf("    -h help\n");
             printf("    -i wordlist.txt words, one word per line in UTF8. If omitted, basic set of english words is used\n");
             printf("    -json output is json probability tree (default)\n");
@@ -122,6 +196,12 @@ int main(int argc, const char * argv[]) {
                 case InputModeResourcesToJSON: {
                     NSData *data = [charTree json];
                     printf("%s\n", data.bytes);
+                    break;
+                }
+                case InputModeResourcesToXML:
+                case InputModeFileToXML: {
+                    NSXMLDocument *doc = makeGraphML(charTree);
+                    printf("%s\n", [doc XMLData].bytes);
                     break;
                 }
                 default: // InputModeResourcesToRandom, InputModeFileToRandom
